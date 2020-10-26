ed108917c3a1e6b3a144fc487d6463fb803869d4
yxia0053 <61861312+yxia0053@users.noreply.github.com>
2020-10-26 11:56:55 +1100

    Add files via upload

---
+++ BookingsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ass01.Models;
+using FIT5032_Week08A.Utils;
+using Microsoft.AspNet.Identity;
+
+namespace ass01.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private AssModel2 db = new AssModel2();
+
+        // GET: Bookings
+        public ActionResult Index()
+        {
+            var bookings = db.Bookings.Include(b => b.Course);
+            return View(bookings.ToList());
+        }
+
+        // GET: Bookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // GET: Bookings/Create?date=YYYY-MM-DD
+        public ActionResult Create(String date)
+        {
+            ViewBag.CourseId = new SelectList(db.Courses, "Id", "CourseName");
+            if (null == date)
+                return RedirectToAction("Index");
+            Booking e = new Booking();
+            var d = date + " 02:00 PM";
+            DateTime covertedDate = Convert.ToDateTime(d);
+            //e.UserId = User.Identity.GetUserId();
+            e.Date = covertedDate;
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Date,UserId,CourseId")] Booking booking)
+        {
+    
+            booking.UserId = User.Identity.GetUserId();
+            ModelState.Clear();
+            TryValidateModel(booking);
+
+            if (ModelState.IsValid)
+            {
+                db.Bookings.Add(booking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseId = new SelectList(db.Courses, "Id", "CourseName", booking.CourseId);
+            return View(booking);
+        }
+
+        // GET: Bookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "Id", "CourseName", booking.CourseId);
+            return View(booking);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Date,Time,UserId,CourseId")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(booking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseId = new SelectList(db.Courses, "Id", "CourseName", booking.CourseId);
+            return View(booking);
+        }
+
+        // GET: Bookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Booking booking = db.Bookings.Find(id);
+            db.Bookings.Remove(booking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public async System.Threading.Tasks.Task<ActionResult> EmailAsync(String Email)
+        {
+            String toEmail = Email;
+            String body = "";
+            EmailSender es = new EmailSender();
+            await es.SendAsync(toEmail, body);
+            return null;
+        }
+
+    }
+}
+
